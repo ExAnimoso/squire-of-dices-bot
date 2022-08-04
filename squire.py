5d1ba4a24b4f4c973cb969af90a5aa645cca3a0d
= <exanimoso@yandex.ru>
2022-08-04 23:34:31 +0300

    Added random for d20

--- squire.py
+++ squire.py
@@ -1,10 +1,14 @@
 import telebot
 import os
+import random
 
 TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['bruh'])
+@bot.message_handler(content_types=['text'])
 def bruh_handler(message):
-    bot.send_message(message.chat.id, 'BRUH')
+    if message.from_user.username == 'ExAnimoso':
+        bot.send_message(message.chat.id, message.text + ' ' + str(random.randint(1, 20)))
+    else:
+        bot.send_message(message.chat.id, 'Возьми д20 в руки и брось на стол. Сообщи чату результат на верхней грани.')
 bot.polling()
